c96cbcb437df095d5cb8808740c82ced3de4cb22
matheushent <matheustosta66@gmail.com>
2021-06-19 11:24:41 -0400

    remove unnecessary .format() call

--- armada_agent/utils/jwt.py
+++ armada_agent/utils/jwt.py
@@ -8,9 +8,7 @@ from armada_agent.settings import SETTINGS
 async def generate_jwt_token(test: bool = True):
 
     proc = await asyncio.create_subprocess_shell(
-        "scontrol token".format(SETTINGS.X_SLURM_USER_NAME)
-        if not test
-        else "juju run --unit slurmctld/3 scontrol token",
+        "scontrol token" if not test else "juju run --unit slurmctld/3 scontrol token",
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE,
     )
