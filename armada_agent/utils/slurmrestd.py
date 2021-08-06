622bb3a36864483176da3bd6902cd34b1ddfcdf3
matheushent <matheustosta66@gmail.com>
2021-08-06 10:55:26 -0400

    create module for slurmrestd related operations

---
+++ armada_agent/utils/slurmrestd.py
@@ -0,0 +1,12 @@
+from armada_agent.settings import SETTINGS
+from armada_agent.utils.jwt import generate_jwt_token
+
+
+async def slurmrestd_header():
+
+    x_slurm_user_token = await generate_jwt_token(test=False)
+
+    return {
+        "X-SLURM-USER-NAME": SETTINGS.X_SLURM_USER_NAME,
+        "X-SLURM-USER-TOKEN": x_slurm_user_token,
+    }
