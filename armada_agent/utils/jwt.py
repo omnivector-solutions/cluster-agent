ff1f41316b8a1efc9897d2879f2a5666d1c404d2
matheushent <matheustosta66@gmail.com>
2021-10-14 16:01:44 -0400

    fix workflow to run on PR with main branch as target and solve linter

--- armada_agent/utils/jwt.py
+++ armada_agent/utils/jwt.py
@@ -2,7 +2,6 @@
 import asyncio
 
 from armada_agent.utils.exception import ProcessExecutionError
-from armada_agent.settings import SETTINGS
 
 
 async def generate_jwt_token():
@@ -10,7 +9,7 @@ async def generate_jwt_token():
     # note the username isn't passed in the scontrol command line
     # it's responsability of who is running the code to run it with the specified user
     proc = await asyncio.create_subprocess_shell(
-        f"scontrol token",
+        "scontrol token",
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE,
     )
