a9793af4107727c31c02299b4bfeb22df9114a03
matheushent <me@mtosta.dev>
2022-04-11 12:07:34 -0400

    set custom config target to the BaseSettingsClass

--- cluster_agent/datastore/interfaces/constants.py
+++ cluster_agent/datastore/interfaces/constants.py
@@ -4,3 +4,8 @@ from pydantic import BaseModel
 class BaseSettingsClass(BaseModel):
 
     is_configured = False
+
+    class Config:
+
+        env_file = ".env"
+        env_prefix = "CLUSTER_AGENT_"
