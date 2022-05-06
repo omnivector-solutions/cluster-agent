3db1f492ba9d0cba5cdb75d862186e3838094c1a
Felipe Schuch <felipe.schuch@pucrs.edu.br>
2022-05-06 15:21:41 -0300

    ARMADA-505 refactoring the sbatch information
    They are now in a propper data structure, and will be liked to slurm API

--- cluster_agent/utils/parser.py
+++ cluster_agent/utils/parser.py
@@ -1,6 +1,9 @@
 from argparse import ArgumentParser
-from typing import List, Iterator
+from collections import namedtuple
 from itertools import chain
+from typing import Iterator, List
+
+from bidict import bidict
 
 _IDENTIFICATION_FLAG = "#SBATCH"
 _INLINE_COMMENT_MARK = "#"
@@ -44,106 +47,129 @@ def _clean_jobscript(jobscript: str) -> Iterator[str]:
     return chain.from_iterable(jobscript_splitted)
 
 
-parser = ArgumentParser()
+SbatchToSlurm = namedtuple("info", "slurm_api sbatch sbatch_short argparser_param")
 
-parser.add_argument("-A", "--account")
-parser.add_argument("--acctg-freq")
-parser.add_argument("-a", "--array")
-parser.add_argument("--batch")
-parser.add_argument("--bb")
-parser.add_argument("--bbf")
-parser.add_argument("-b", "--begin")
-parser.add_argument("-D", "--chdir")
-parser.add_argument("--cluster-constraint")
-parser.add_argument("-M", "--clusters")
-parser.add_argument("--comment")
-parser.add_argument("-C", "--constraint")
-parser.add_argument("--container")
-parser.add_argument("--contiguous", action="store_const", const=True)
-parser.add_argument("-S", "--core-spec")
-parser.add_argument("--cores-per-socket")
-parser.add_argument("--cpu-freq")
-parser.add_argument("--cpus-per-gpu")
-parser.add_argument("-c", "--cpus-per-task")
-parser.add_argument("--deadline")
-parser.add_argument("--delay-boot")
-parser.add_argument("-d", "--dependency")
-parser.add_argument("-m", "--distribution")
-parser.add_argument("-e", "--error")
-parser.add_argument("-x", "--exclude")
-parser.add_argument("--exclusive")
-parser.add_argument("--export")
-parser.add_argument("--export-file")
-parser.add_argument("-B", "--extra-node-info")
-parser.add_argument("--get-user-env")
-parser.add_argument("--gid")
-parser.add_argument("--gpu-bind")
-parser.add_argument("--gpu-freq")
-parser.add_argument("-G", "--gpus")
-parser.add_argument("--gpus-per-node")
-parser.add_argument("--gpus-per-socket")
-parser.add_argument("--gpus-per-task")
-parser.add_argument("--gres")
-parser.add_argument("--gres-flags")
-parser.add_argument("--hint")
-parser.add_argument("-H", "--hold", action="store_const", const=True)
-parser.add_argument("--ignore-pbs", action="store_const", const=True)
-parser.add_argument("-i", "--input")
-parser.add_argument("-J", "--job-name")
-parser.add_argument("--kill-on-invalid-dep")
-parser.add_argument("-L", "--licenses")
-parser.add_argument("--mail-type")
-parser.add_argument("--mail-user")
-parser.add_argument("--mcs-label")
-parser.add_argument("--mem")
-parser.add_argument("--mem-bind")
-parser.add_argument("--mem-per-cpu")
-parser.add_argument("--mem-per-gpu")
-parser.add_argument("--mincpus")
-parser.add_argument("--network")
-parser.add_argument("--nice")
-parser.add_argument("-k", "--no-kill", action="store_const", const=True)
-parser.add_argument("--no-requeue", action="store_const", const=True)
-parser.add_argument("-F", "--nodefile")
-parser.add_argument("-w", "--nodelist")
-parser.add_argument("-N", "--nodes")
-parser.add_argument("-n", "--ntasks")
-parser.add_argument("--ntasks-per-core")
-parser.add_argument("--ntasks-per-gpu")
-parser.add_argument("--ntasks-per-node")
-parser.add_argument("--ntasks-per-socket")
-parser.add_argument("--open-mode")
-parser.add_argument("-o", "--output")
-parser.add_argument("-O", "--overcommit", action="store_const", const=True)
-parser.add_argument("-s", "--oversubscribe", action="store_const", const=True)
-parser.add_argument("--parsable", action="store_const", const=True)
-parser.add_argument("-p", "--partition")
-parser.add_argument("--power")
-parser.add_argument("--priority")
-parser.add_argument("--profile")
-parser.add_argument("--propagate")
-parser.add_argument("-q", "--qos")
-parser.add_argument("-Q", "--quiet", action="store_const", const=True)
-parser.add_argument("--reboot", action="store_const", const=True)
-# TODO: link it with --no-requeue
-parser.add_argument("--requeue", action="store_const", const=True)
-parser.add_argument("--reservation")
-parser.add_argument("--signal")
-parser.add_argument("--sockets-per-node")
-parser.add_argument("--spread-job", action="store_const", const=True)
-parser.add_argument("--switches")
-parser.add_argument("--test-only", action="store_const", const=True)
-parser.add_argument("--thread-spec")
-parser.add_argument("--threads-per-core")
-parser.add_argument("-t", "--time")
-parser.add_argument("--time-min")
-parser.add_argument("--tmp")
-parser.add_argument("--uid")
-parser.add_argument("--usage", action="store_const", const=True)
-parser.add_argument("--use-min-nodes", action="store_const", const=True)
-parser.add_argument("-v", "--verbose", action="store_const", const=True)
-parser.add_argument("-V", "--version", action="store_const", const=True)
-parser.add_argument("-W", "--wait", action="store_const", const=True)
-parser.add_argument("--wait-all-nodes")
-parser.add_argument("--wckey")
-parser.add_argument("--wrap")
+sbatch_to_slurm = [
+    SbatchToSlurm("", "--account", "-A", {}),
+    SbatchToSlurm("", "--acctg-freq", "", {}),
+    SbatchToSlurm("", "--array", "-a", {}),
+    SbatchToSlurm("", "--batch", "", {}),
+    SbatchToSlurm("", "--bb", "", {}),
+    SbatchToSlurm("", "--bbf", "", {}),
+    SbatchToSlurm("", "--begin", "-b", {}),
+    SbatchToSlurm("", "--chdir", "-D", {}),
+    SbatchToSlurm("", "--cluster-constraint", "", {}),
+    SbatchToSlurm("", "--clusters", "-M", {}),
+    SbatchToSlurm("", "--comment", "", {}),
+    SbatchToSlurm("", "--constraint", "-C", {}),
+    SbatchToSlurm("", "--container", "", {}),
+    SbatchToSlurm("", "--contiguous", "", dict(action="store_const", const=True)),
+    SbatchToSlurm("", "--core-spec", "-S", {}),
+    SbatchToSlurm("", "--cores-per-socket", "", {}),
+    SbatchToSlurm("", "--cpu-freq", "", {}),
+    SbatchToSlurm("", "--cpus-per-gpu", "", {}),
+    SbatchToSlurm("", "--cpus-per-task", "-c", {}),
+    SbatchToSlurm("", "--deadline", "", {}),
+    SbatchToSlurm("", "--delay-boot", "", {}),
+    SbatchToSlurm("", "--dependency", "-d", {}),
+    SbatchToSlurm("", "--distribution", "-m", {}),
+    SbatchToSlurm("", "--error", "-e", {}),
+    SbatchToSlurm("", "--exclude", "-x", {}),
+    SbatchToSlurm("", "--exclusive", "", {}),
+    SbatchToSlurm("", "--export", "", {}),
+    SbatchToSlurm("", "--export-file", "", {}),
+    SbatchToSlurm("", "--extra-node-info", "-B", {}),
+    SbatchToSlurm("", "--get-user-env", "", {}),
+    SbatchToSlurm("", "--gid", "", {}),
+    SbatchToSlurm("", "--gpu-bind", "", {}),
+    SbatchToSlurm("", "--gpu-freq", "", {}),
+    SbatchToSlurm("", "--gpus", "-G", {}),
+    SbatchToSlurm("", "--gpus-per-node", "", {}),
+    SbatchToSlurm("", "--gpus-per-socket", "", {}),
+    SbatchToSlurm("", "--gpus-per-task", "", {}),
+    SbatchToSlurm("", "--gres", "", {}),
+    SbatchToSlurm("", "--gres-flags", "", {}),
+    SbatchToSlurm("", "--hint", "", {}),
+    SbatchToSlurm("", "--hold", "-H", dict(action="store_const", const=True)),
+    SbatchToSlurm("", "--ignore-pbs", "", dict(action="store_const", const=True)),
+    SbatchToSlurm("", "--input", "-i", {}),
+    SbatchToSlurm("", "--job-name", "-J", {}),
+    SbatchToSlurm("", "--kill-on-invalid-dep", "", {}),
+    SbatchToSlurm("", "--licenses", "-L", {}),
+    SbatchToSlurm("", "--mail-type", "", {}),
+    SbatchToSlurm("", "--mail-user", "", {}),
+    SbatchToSlurm("", "--mcs-label", "", {}),
+    SbatchToSlurm("", "--mem", "", {}),
+    SbatchToSlurm("", "--mem-bind", "", {}),
+    SbatchToSlurm("", "--mem-per-cpu", "", {}),
+    SbatchToSlurm("", "--mem-per-gpu", "", {}),
+    SbatchToSlurm("", "--mincpus", "", {}),
+    SbatchToSlurm("", "--network", "", {}),
+    SbatchToSlurm("", "--nice", "", {}),
+    SbatchToSlurm("", "--no-kill", "-k", dict(action="store_const", const=True)),
+    SbatchToSlurm("", "--no-requeue", "", dict(action="store_const", const=True)),
+    SbatchToSlurm("", "--nodefile", "-F", {}),
+    SbatchToSlurm("", "--nodelist", "-w", {}),
+    SbatchToSlurm("", "--nodes", "-N", {}),
+    SbatchToSlurm("", "--ntasks", "-n", {}),
+    SbatchToSlurm("", "--ntasks-per-core", "", {}),
+    SbatchToSlurm("", "--ntasks-per-gpu", "", {}),
+    SbatchToSlurm("", "--ntasks-per-node", "", {}),
+    SbatchToSlurm("", "--ntasks-per-socket", "", {}),
+    SbatchToSlurm("", "--open-mode", "", {}),
+    SbatchToSlurm("", "--output", "-o", {}),
+    SbatchToSlurm("", "--overcommit", "-O", dict(action="store_const", const=True)),
+    SbatchToSlurm("", "--oversubscribe", "-s", dict(action="store_const", const=True)),
+    SbatchToSlurm("", "--parsable", "", dict(action="store_const", const=True)),
+    SbatchToSlurm("", "--partition", "-p", {}),
+    SbatchToSlurm("", "--power", "", {}),
+    SbatchToSlurm("", "--priority", "", {}),
+    SbatchToSlurm("", "--profile", "", {}),
+    SbatchToSlurm("", "--propagate", "", {}),
+    SbatchToSlurm("", "--qos", "-q", {}),
+    SbatchToSlurm("", "--quiet", "-Q", dict(action="store_const", const=True)),
+    SbatchToSlurm("", "--reboot", "", dict(action="store_const", const=True)),
+    SbatchToSlurm("", "--requeue", "", dict(action="store_const", const=True)),
+    SbatchToSlurm("", "--reservation", "", {}),
+    SbatchToSlurm("", "--signal", "", {}),
+    SbatchToSlurm("", "--sockets-per-node", "", {}),
+    SbatchToSlurm("", "--spread-job", "", dict(action="store_const", const=True)),
+    SbatchToSlurm("", "--switches", "", {}),
+    SbatchToSlurm("", "--test-only", "", dict(action="store_const", const=True)),
+    SbatchToSlurm("", "--thread-spec", "", {}),
+    SbatchToSlurm("", "--threads-per-core", "", {}),
+    SbatchToSlurm("", "--time", "-t", {}),
+    SbatchToSlurm("", "--time-min", "", {}),
+    SbatchToSlurm("", "--tmp", "", {}),
+    SbatchToSlurm("", "--uid", "", {}),
+    SbatchToSlurm("", "--usage", "", dict(action="store_const", const=True)),
+    SbatchToSlurm("", "--use-min-nodes", "", dict(action="store_const", const=True)),
+    SbatchToSlurm("", "--verbose", "-v", dict(action="store_const", const=True)),
+    SbatchToSlurm("", "--version", "-V", dict(action="store_const", const=True)),
+    SbatchToSlurm("", "--wait", "-W", dict(action="store_const", const=True)),
+    SbatchToSlurm("", "--wait-all-nodes", "", {}),
+    SbatchToSlurm("", "--wckey", "", {}),
+    SbatchToSlurm("", "--wrap", "", {}),
+]
+
+
+def build_parser():
+    parser = ArgumentParser()
+    for item in sbatch_to_slurm:
+        args = (i for i in (item.sbatch_short, item.sbatch) if i)
+        parser.add_argument(*args, **item.argparser_param)
+    return parser
+
+
+def build_mapping_sbatch_to_slurm():
+    mapping = bidict()
+
+    for item in sbatch_to_slurm:
+        if item.slurm_api:
+            mapping[item.sbatch.lstrip("-")] = item.slurm_api
+
+    return mapping
+
+
+parser = build_parser()
+mapping_sbatch_to_slurm = build_mapping_sbatch_to_slurm()
