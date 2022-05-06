83df9ff62037feb336a1acb015714392e9ef4876
Felipe Schuch <felipe.schuch@pucrs.edu.br>
2022-05-06 11:22:13 -0300

    ARMADA-505 refactoring job-script manipulation

--- cluster_agent/utils/parser.py
+++ cluster_agent/utils/parser.py
@@ -1,3 +1,6 @@
+from typing import List, Iterator
+from itertools import chain
+
 _IDENTIFICATION_FLAG = "#SBATCH"
 _INLINE_COMMENT_MARK = "#"
 
@@ -17,17 +20,24 @@ def _clean_line(line: str) -> str:
     then remove the inline comment mark and anything
     after it, and finally strip white spaces at both sides
     """
-    return line.lstrip(_IDENTIFICATION_FLAG).split(_INLINE_COMMENT_MARK, 1)[0].strip()
+    return line.lstrip(_IDENTIFICATION_FLAG).split(_INLINE_COMMENT_MARK)[0].strip()
 
 
-def _clean_jobscript(jobscript: str) -> map:
+def _split_line(line: str) -> List[str]:
+    if "=" in line:
+        return line.split("=")
+    return line.split()
+
+
+def _clean_jobscript(jobscript: str) -> Iterator[str]:
     """
-    Transform a job script string by filtering only the lines that
-    start with the identification flag and mapping a cleaning
-    procedure to them, in
-    order to remove the identification flag, remove inline comments,
-    and strip extra white spaces
+    Transform a job script string by filtering only the lines that start with
+    the identification flag and mapping a cleaning procedure to them, in order
+    to remove the identification flag, remove inline comments, and strip extra
+    white spaces. Finally, split each pair of parameter/value and chain them
+    in a single iterator.
     """
-    jobscript_filtered = filter(_flagged_line, jobscript.split("\n"))
+    jobscript_filtered = filter(_flagged_line, jobscript.splitlines())
     jobscript_cleaned = map(_clean_line, jobscript_filtered)
-    return jobscript_cleaned
+    jobscript_splitted = map(_split_line, jobscript_cleaned)
+    return chain.from_iterable(jobscript_splitted)
