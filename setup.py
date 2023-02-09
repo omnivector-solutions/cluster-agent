fc05c635e8149e31ac541904d774846b47e2df7d
fschuch <felipe@omnivector.solutions>
2023-02-09 13:17:21 -0300

    bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import dirname, join
 
 here = dirname(__file__)
 
-_VERSION = "2.2.0"
+_VERSION = "2.2.1"
 
 setup(
     name="ovs-cluster-agent",
@@ -18,21 +18,24 @@ setup(
     download_url="https://github.com/omnivector-solutions/ovs-cluster-agent/dist/cluster-agent-"
     + _VERSION
     + "tar.gz",
-    install_requires=list(map(lambda string: string.strip("\n"), open("requirements.txt", "r"))),
-    extras_require=dict(dev=[
-        "pytest==7.1.0",
-        "pytest-mock==3.7.0",
-        "respx==0.19.2",
-        "asynctest==0.13.0",
-        "pytest-asyncio==0.18.2",
-        "black==22.3.0",
-        "flake8==4.0.1",
-        "pytest-env==0.6.2",
-        "pytest-random-order==1.0.4",
-        "pytest-cov==3.0.0",
-        "freezegun==1.2.2",
-
-    ]),
+    install_requires=list(
+        map(lambda string: string.strip("\n"), open("requirements.txt", "r"))
+    ),
+    extras_require=dict(
+        dev=[
+            "pytest==7.1.0",
+            "pytest-mock==3.7.0",
+            "respx==0.19.2",
+            "asynctest==0.13.0",
+            "pytest-asyncio==0.18.2",
+            "black==22.3.0",
+            "flake8==4.0.1",
+            "pytest-env==0.6.2",
+            "pytest-random-order==1.0.4",
+            "pytest-cov==3.0.0",
+            "freezegun==1.2.2",
+        ]
+    ),
     packages=find_packages(),
     keywords=["armada", "hpc"],
     classifiers=[
